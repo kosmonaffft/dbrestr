2bac0c5ea79aec0986830ab5870e289fead42246
Anton V. Kirilchik <kosmonaffft@gmail.com>
2019-09-17 16:52:51 +0300

    Empty SpringBoot application...

---
+++ src/main/kotlin/xyz/kosmonaffft/dbrestr/DbrestrApplication.kt
@@ -0,0 +1,11 @@
+package xyz.kosmonaffft.dbrestr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbrestrApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbrestrApplication>(*args)
+}
