9ec81ed9f17b20000828ae18e4f118b1c45dabec
Anton V. Kirilchik <kosmonaffft@gmail.com>
2019-09-18 18:34:53 +0300

    Improve metadata generation...

--- src/main/kotlin/xyz/kosmonaffft/dbrestr/service/MetadataService.kt
+++ src/main/kotlin/xyz/kosmonaffft/dbrestr/service/MetadataService.kt
@@ -17,11 +17,12 @@ package xyz.kosmonaffft.dbrestr.service
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.media.ObjectSchema
+import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.stereotype.Service
 import xyz.kosmonaffft.dbrestr.configuration.ConfigurationProperties
 import java.sql.Connection
-import java.sql.DatabaseMetaData
 import java.sql.ResultSet
 import javax.sql.DataSource
 
@@ -42,41 +43,51 @@ class MetadataService(private val dataSource: DataSource,
     }
 
     private fun generateComponents(connection: Connection): Components {
+        val result = Components()
         val databaseMetadata = connection.metaData
-        val schemas: ArrayList<String> = getSchemasNames(databaseMetadata)
-        val tables: ArrayList<String> = getTablesNames(databaseMetadata, schemas)
 
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        val schemaAction: (ResultSet) -> Unit = { schemasResultSet ->
+            while (schemasResultSet.next()) {
+                val schemaName = schemasResultSet.getString("TABLE_SCHEM")
 
-    private fun getTablesNames(databaseMetadata: DatabaseMetaData, schemas: java.util.ArrayList<String>): ArrayList<String> {
-        val result = ArrayList<String>()
-        schemas.forEach { schemaName ->
-            databaseMetadata.getTables(null, schemaName, null, arrayOf("TABLE")).use { tablesResultSet ->
-                while (tablesResultSet.next()) {
-                    val tableName = tablesResultSet.getString("TABLE_NAME")
-                    result.add(tableName)
+                databaseMetadata.getTables(null, schemaName, null, arrayOf("TABLE")).use { tablesResultSet ->
+                    while (tablesResultSet.next()) {
+                        val tableName = tablesResultSet.getString("TABLE_NAME")
+                        val fullTableName = "${schemaName}/${tableName}"
+
+                        val oaTableSchema = ObjectSchema()
+                                .name(fullTableName)
+
+                        databaseMetadata.getColumns(null, schemaName, tableName, null).use { columnsResultSet ->
+                            while (columnsResultSet.next()) {
+                                val columnName = columnsResultSet.getString("COLUMN_NAME")
+                                val columnTypeName = columnsResultSet.getString("TYPE_NAME")
+                                val columnNullable = columnsResultSet.getShort("NULLABLE")
+                                val columnFormat = jdbcToOpenApiFormat(columnTypeName)
+
+                                val oaColumnSchema = Schema<Any>()
+                                        .title(columnName)
+                                        .type(jdbcToOpenApiType(columnTypeName))
+                                        .nullable(columnNullable != 0.toShort())
+                                if (columnFormat != null) {
+                                    oaColumnSchema.format(columnFormat)
+                                }
+
+                                oaTableSchema.addProperties(columnName, oaColumnSchema)
+                            }
+                        }
+
+                        result.addSchemas(fullTableName, oaTableSchema)
+                    }
                 }
             }
         }
 
-        return result
-    }
-
-    private fun getSchemasNames(databaseMetadata: DatabaseMetaData): ArrayList<String> {
-        val result = ArrayList<String>()
-        val schemasExtractor: (ResultSet) -> Unit = { schemasResultSet ->
-            while (schemasResultSet.next()) {
-                val schemaName = schemasResultSet.getString("TABLE_SCHEM")
-                result.add(schemaName)
-            }
-        }
-
         if (configurationProperties.schemas.isEmpty()) {
-            databaseMetadata.schemas.use(schemasExtractor)
+            databaseMetadata.schemas.use(schemaAction)
         } else {
             configurationProperties.schemas.forEach { schemaName ->
-                databaseMetadata.getSchemas(null, schemaName).use(schemasExtractor)
+                databaseMetadata.getSchemas(null, schemaName).use(schemaAction)
             }
         }
 
@@ -92,5 +103,41 @@ class MetadataService(private val dataSource: DataSource,
         return Info()
                 .title("dbrestr")
                 .version("1.0.0")
+    }
+}
+
+fun jdbcToOpenApiType(jdbcType: String): String {
+    return when (jdbcType) {
+        "text", "bytea", "varchar", "date", "timestamp", "timestamptz", "json", "jsonb", "uuid" -> "string"
+
+        "int4", "int8", "serial", "bigserial" -> "integer"
+
+        "float4", "float8" -> "number"
+
+        "bool" -> "boolean"
+
+        else -> jdbcType
+    }
+}
+
+fun jdbcToOpenApiFormat(jdbcType: String): String? {
+    return when (jdbcType) {
+        "int4", "serial" -> "int32"
+
+        "int8", "bigserial" -> "int64"
+
+        "float4" -> "float"
+
+        "float8" -> "double"
+
+        "bytea" -> "binary"
+
+        "date" -> "date"
+
+        "timestamp", "timestamptz" -> "date-time"
+
+        "uuid" -> "uuid"
+
+        else -> null
     }
 }
